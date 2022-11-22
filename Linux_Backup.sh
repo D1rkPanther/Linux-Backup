5ccde1aa3a32a9e55c21ad24f4753f3f9446817f
abhi9300 <76582024+abhi9300@users.noreply.github.com>
2022-11-22 11:40:17 +0530

    Add files via upload

---
+++ Linux_Backup.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+echo "Creating Directory in /mnt/backup"
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+echo  "."
+
+sudo mkdir /mnt/backup
+
+echo "Finished"
+
+# Backup folders  
+backup_files="/home /var/ /etc /root /boot /opt"
+
+# Backup destination path.
+dest="/mnt/backup"
+
+# Create archive filename.
+day=$(date +%d-%B-%Y)
+hostname=$(hostname)
+archive_file="$hostname-$day.tgz"
+
+
+echo "Backing up $backup_files to $dest/$archive_file"
+date
+echo
+echo 
+
+
+# Backup the files using tar.
+sudo tar czf $dest/$archive_file $backup_files
+
+# Print end status message.
+echo
+echo "Backup finished"
+date
+
+# $dest to check file sizes.
+ls -lh $dest
+
+echo "Thanks! Have a Nice Day"
